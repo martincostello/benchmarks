1293cb14bf666fb52b2db52abf3ab811e0ab926f
github-action-benchmark <github@users.noreply.github.com>
2024-08-10 16:50:00 +0000

    add OpenAPI Extensions Benchmarks (benchmarkdotnet) benchmark result for 3d2bd5330a83ead9b339a9f992c2b08e3aaa1b4b

--- openapi-extensions/data.js
+++ openapi-extensions/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723308111544,
+  "lastUpdate": 1723308600630,
   "repoUrl": "https://github.com/martincostello/openapi-extensions",
   "entries": {
     "OpenAPI Extensions Benchmarks": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 15498431.166666666,
             "unit": "ns",
             "range": "± 2636097.273566227"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin@martincostello.com",
+            "name": "martincostello",
+            "username": "martincostello"
+          },
+          "committer": {
+            "email": "martin@martincostello.com",
+            "name": "Martin Costello",
+            "username": "martincostello"
+          },
+          "distinct": true,
+          "id": "3d2bd5330a83ead9b339a9f992c2b08e3aaa1b4b",
+          "message": "Update .NET SDK\n\nUpdate to the latest build of .NET 9 preview 7.",
+          "timestamp": "2024-08-10T17:48:13+01:00",
+          "tree_id": "752738e44679ecd043466a431868af4e67ee0219",
+          "url": "https://github.com/martincostello/openapi-extensions/commit/3d2bd5330a83ead9b339a9f992c2b08e3aaa1b4b"
+        },
+        "date": 1723308600347,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "MartinCostello.OpenApi.OpenApiBenchmarks.GetOpenApiDocumentJson",
+            "value": 12716391.276041666,
+            "unit": "ns",
+            "range": "± 577333.2546882937"
+          },
+          {
+            "name": "MartinCostello.OpenApi.OpenApiBenchmarks.GetOpenApiDocumentYaml",
+            "value": 18830381.552083332,
+            "unit": "ns",
+            "range": "± 8689869.982274253"
           }
         ]
       }
