8bcc1412117bfb698c7af867457216c79cfd648d
martincostello <martin@martincostello.com>
2024-08-12 13:55:31 +0100

    Build table dynamically
    Build the index table dynamically, rather than copy-pasta.

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+(function () {
+  const repos = [
+    'adventofcode',
+    'api',
+    'aspnetcore-openapi',
+    'openapi-extensions',
+    'project-euler',
+    'website',
+  ];
+  const tableBody = document.getElementById('table-body');
+  for (const repo of repos) {
+    const row = document.createElement('tr');
+
+    const repoColumn = document.createElement('td');
+
+    const repoIcon = document.createElement('span');
+    repoIcon.classList.add('fa-brands');
+    repoIcon.classList.add('fa-github');
+    repoIcon.setAttribute('aria-hidden', 'true');
+
+    const repoLink = document.createElement('a');
+    repoLink.href = `https://github.com/martincostello/${repo}`;
+    repoLink.target = '_blank';
+    repoLink.appendChild(repoIcon);
+    repoLink.appendChild(document.createTextNode(repo));
+
+    repoColumn.appendChild(repoLink);
+    row.appendChild(repoColumn);
+
+    const resultsColumn = document.createElement('td');
+
+    const resultsIcon = document.createElement('span');
+    resultsIcon.classList.add('fa-solid');
+    resultsIcon.classList.add('fa-chart-line');
+    resultsIcon.setAttribute('aria-hidden', 'true');
+
+    const resultsLink = document.createElement('a');
+    resultsLink.href = `https://github.com/martincostello/${repo}`;
+    resultsLink.role = 'button';
+    resultsLink.classList.add('btn');
+    resultsLink.classList.add('btn-primary');
+    resultsLink.appendChild(resultsIcon);
+    resultsLink.appendChild(document.createTextNode(' Results \u00bb'));
+
+    resultsColumn.appendChild(resultsLink);
+    row.appendChild(resultsColumn);
+
+    tableBody.appendChild(row);
+  }
+})();
