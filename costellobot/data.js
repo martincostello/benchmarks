2bea3ed5a528250d0b2c9c30aaa6c2aa1280bccd
github-action-benchmark <github@users.noreply.github.com>
2024-08-13 17:06:49 +0000

    add Costellobot (benchmarkdotnet) benchmark result for 36d72c5ec00b996cb2a245a33c84693cb5b52e7e

--- costellobot/data.js
+++ costellobot/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723539389952,
+  "lastUpdate": 1723568809523,
   "repoUrl": "https://github.com/martincostello/costellobot",
   "entries": {
     "Costellobot": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 313472.8818359375,
             "unit": "ns",
             "range": "± 1887.878185704217"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "102549341+costellobot@users.noreply.github.com",
+            "name": "costellobot",
+            "username": "costellobot"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "36d72c5ec00b996cb2a245a33c84693cb5b52e7e",
+          "message": "Update .NET SDK to 8.0.400 (#1619)\n\n* Update .NET SDK\n\nUpdate .NET SDK to version 8.0.400.\n\n---\nupdated-dependencies:\n- dependency-name: Microsoft.NET.Sdk\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: costellobot <102549341+costellobot@users.noreply.github.com>\n\n* Bump .NET NuGet packages\n\nBumps .NET dependencies to their latest versions for the .NET 8.0.400 SDK.\n\nBumps Microsoft.AspNetCore.AzureAppServices.HostingStartup from 8.0.7 to 8.0.8.\nBumps Microsoft.AspNetCore.Mvc.Testing from 8.0.7 to 8.0.8.\n\n---\nupdated-dependencies:\n- dependency-name: Microsoft.AspNetCore.AzureAppServices.HostingStartup\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n- dependency-name: Microsoft.AspNetCore.Mvc.Testing\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: costellobot <102549341+costellobot@users.noreply.github.com>\n\n---------\n\nSigned-off-by: costellobot <102549341+costellobot@users.noreply.github.com>",
+          "timestamp": "2024-08-13T17:01:54Z",
+          "tree_id": "3f50bf81d45ee0710834b72a7949360f5f1bb064",
+          "url": "https://github.com/martincostello/costellobot/commit/36d72c5ec00b996cb2a245a33c84693cb5b52e7e"
+        },
+        "date": 1723568809240,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "MartinCostello.Costellobot.Benchmarks.AppBenchmarks.Root",
+            "value": 595959.3607271635,
+            "unit": "ns",
+            "range": "± 1451.6032241480434"
+          },
+          {
+            "name": "MartinCostello.Costellobot.Benchmarks.AppBenchmarks.Version",
+            "value": 256387.15455729168,
+            "unit": "ns",
+            "range": "± 4083.1629576802397"
+          },
+          {
+            "name": "MartinCostello.Costellobot.Benchmarks.AppBenchmarks.Webhook",
+            "value": 304830.7643229167,
+            "unit": "ns",
+            "range": "± 6295.365453356996"
           }
         ]
       }
