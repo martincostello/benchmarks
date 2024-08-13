32e53ec9f9397d9301c35146a61d9f58be2b682f
github-action-benchmark <github@users.noreply.github.com>
2024-08-13 09:38:24 +0000

    add ASP.NET Core OpenAPI (benchmarkdotnet) benchmark result for 57eae15e2f13af00a9af9e670eea047016def2da

--- aspnetcore-openapi/data.js
+++ aspnetcore-openapi/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723466485342,
+  "lastUpdate": 1723541904442,
   "repoUrl": "https://github.com/martincostello/aspnetcore-openapi",
   "entries": {
     "ASP.NET Core OpenAPI": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 7988972.505208333,
             "unit": "ns",
             "range": "± 377021.0010923228"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin@martincostello.com",
+            "name": "martincostello",
+            "username": "martincostello"
+          },
+          "committer": {
+            "email": "martin@martincostello.com",
+            "name": "Martin Costello",
+            "username": "martincostello"
+          },
+          "distinct": true,
+          "id": "57eae15e2f13af00a9af9e670eea047016def2da",
+          "message": "Bump dependencies\n\nUpdate Microsoft.Playwright and Verify.Xunit to their latest versions.",
+          "timestamp": "2024-08-13T10:36:12+01:00",
+          "tree_id": "481540913706dc4944204797fda2d3e02937c1d3",
+          "url": "https://github.com/martincostello/aspnetcore-openapi/commit/57eae15e2f13af00a9af9e670eea047016def2da"
+        },
+        "date": 1723541904148,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "TodoApp.OpenApiBenchmarks.AspNetCore",
+            "value": 11872583.333333334,
+            "unit": "ns",
+            "range": "± 523728.78378956195"
+          },
+          {
+            "name": "TodoApp.OpenApiBenchmarks.NSwag",
+            "value": 13229548.036458334,
+            "unit": "ns",
+            "range": "± 397949.0227714181"
+          },
+          {
+            "name": "TodoApp.OpenApiBenchmarks.Swashbuckle",
+            "value": 7968058.833333333,
+            "unit": "ns",
+            "range": "± 93049.31423533752"
           }
         ]
       }
