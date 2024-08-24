1721fa5fae7e1f8760e5fb59dea3742377e3d27e
martincostello <martin@martincostello.com>
2024-08-24 14:48:40 +0100

    Add Blazor WASM skeleton
    Add skeleton of the solution to convert to a Blazor WebAssembly app.

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using MartinCostello.Benchmarks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(
+    (provider) =>
+        new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
