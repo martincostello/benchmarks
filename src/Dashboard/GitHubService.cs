d5cbe76674af6c3307b994ed80826f91087d80ed
martincostello <martin@martincostello.com>
2024-08-25 09:51:31 +0100

    Fix authentication state
    - Use `GitHubService` to flow the current authentication state.
    - Remove invalid `async void` handler.
    - Require GitHub Enterprise users to have a token.
    - Add feedback for invalid tokens.

--- src/Dashboard/GitHubService.cs
+++ src/Dashboard/GitHubService.cs
@@ -2,39 +2,95 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using Blazored.LocalStorage;
+using MartinCostello.Benchmarks.Models;
 
 namespace MartinCostello.Benchmarks;
 
 /// <summary>
 /// A class representing a service for interacting with GitHub.
 /// </summary>
-public sealed class GitHubService(ISyncLocalStorageService localStorage)
+public sealed class GitHubService(
+    GitHubClient client,
+    ISyncLocalStorageService localStorage)
 {
     private const string TokenKey = "github-token";
 
     /// <summary>
-    /// Gets a value indicating whether the user is authenticated with GitHub.
+    /// Raised when the GitHub user changes.
     /// </summary>
-    public bool IsAuthenticated => !string.IsNullOrEmpty(GetToken());
+    public event EventHandler<GitHubUserChangedEventArgs>? OnUserChanged;
 
     /// <summary>
-    /// Stores the specified GitHub token for the current user as an asynchronous operation.
+    /// Gets the current GitHub user, if any.
     /// </summary>
-    /// <param name="token">The token to store.</param>
+    public GitHubUser? CurrentUser { get; private set; }
+
+    /// <summary>
+    /// Gets a value indicating whether has a GitHub token configured.
+    /// </summary>
+    public bool HasToken => !string.IsNullOrEmpty(localStorage.GetItemAsString(TokenKey));
+
+    /// <summary>
+    /// Signs in the user with the specified GitHub token as an asynchronous operation.
+    /// </summary>
+    /// <param name="token">The GitHub token to sign in with.</param>
     /// <param name="cancellationToken">The optional <see cref="CancellationToken"/> to use.</param>
     /// <returns>
-    /// A <see cref="Task"/> representing the asynchronous operation to store the token.
+    /// A <see cref="Task"/> representing the asynchronous operation to sign in that returns
+    /// <see langword="true"/> if the specified token is valid; otherwise <see langword="false"/>.
     /// </returns>
-    public async Task StoreTokenAsync(string token, CancellationToken cancellationToken = default)
+    public async Task<bool> SignInAsync(string token, CancellationToken cancellationToken = default)
+    {
+        localStorage.SetItemAsString(TokenKey, token);
+
+        if (await VerifyTokenAsync(cancellationToken))
+        {
+            OnUserChanged?.Invoke(this, new(CurrentUser));
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Signs out the user as an asynchronous operation.
+    /// </summary>
+    /// <param name="cancellationToken">The optional <see cref="CancellationToken"/> to use.</param>
+    /// <returns>
+    /// A <see cref="Task"/> representing the asynchronous operation to sign out.
+    /// </returns>
+    public async Task SignOutAsync(CancellationToken cancellationToken = default)
     {
         // TODO Make this async with async local storage
         cancellationToken.ThrowIfCancellationRequested();
 
-        localStorage.SetItemAsString(TokenKey, token);
+        localStorage.SetItemAsString(TokenKey, string.Empty);
+
+        CurrentUser = null;
+        OnUserChanged?.Invoke(this, new(null));
 
         await Task.CompletedTask;
     }
 
-    private string? GetToken()
-        => localStorage.GetItemAsString(TokenKey);
+    /// <summary>
+    /// Verifies the current user's access token as an asynchronous operation.
+    /// </summary>
+    /// <param name="cancellationToken">The optional <see cref="CancellationToken"/> to use.</param>
+    /// <returns>
+    /// A <see cref="Task"/> representing the asynchronous operation to verify the token
+    /// that returns <see langword="true"/> if the token is valid; otherwise <see langword="false"/>.
+    /// </returns>
+    public async Task<bool> VerifyTokenAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            CurrentUser = await client.GetUserAsync(cancellationToken);
+            return true;
+        }
+        catch (HttpRequestException)
+        {
+            await SignOutAsync(cancellationToken);
+            return false;
+        }
+    }
 }
